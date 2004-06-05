ab0f90494f2b808b8c06aff42fea9efe07d60a10
Sebastian Stein <seb.kde@hpfsc.de>
2004-06-05 04:51:01 +0000

    fix bug #82863: unbreak compilation for gcc 2.95 Thanks to Michael Nottebrock for pointing to this problem!
    svn path=/trunk/kdeedu/kbruch/; revision=317825

--- src/mainqtwidget.cpp
+++ src/mainqtwidget.cpp
@@ -125,7 +125,7 @@ void MainQtWidget::readOptions()
 	m_maxMainDenominator = SettingsClass::max_main_denominator();
 
 	/* make sure that we can load config files with corrupted values */
-	if (m_mulDiv == true and pow(2, m_nrRatios) > m_maxMainDenominator)
+	if (m_mulDiv == true && pow(2, m_nrRatios) > m_maxMainDenominator)
 	{
 		m_nrRatios = 2;
 		m_maxMainDenominator = 10;
