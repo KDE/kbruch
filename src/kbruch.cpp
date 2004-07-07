d2553c4c35062d9ffb02a55fd98e4d61739bc727
Sebastian Stein <seb.kde@hpfsc.de>
2004-07-07 12:52:50 +0000

    text mode removed
    svn path=/trunk/kdeedu/kbruch/; revision=326583

--- src/kbruch.cpp
+++ src/kbruch.cpp
@@ -19,287 +19,10 @@
 
 #include "version.h"
 
-/* standard C++ library includes */
-#include <math.h>
-#include <stdlib.h>
-#include <time.h>
-
-#include <iostream>
-
-static QTextStream output(stdout, IO_ReadWrite);
-
-/* show the main menu for a text gui */
-char show_main_txt()
-{
-	char chrInput;
-
-	CLEAR; /* clear the console */
-	output << "\tKBruch " << KBRUCH_VERSION << " " << DATE << endl;
-	output << "\n\n\tWhat do you want to do:" << endl;
-	output << "\n\n\t\t[a]ddition/subtraction" << endl;
-	output << "\n\t\t[m]ultiplication/division" << endl;
-	output << "\n\t\t[t]ask with all operations (default)" << endl;
-	output << "\n\t\t[r]ead about the program" << endl;
-	output << "\n\t\t[e]xit program" << endl;
-	output << "\n\n\tInput: ";
-
-	/* check, if the user entered a valid input */
-	if (!(std::cin >> chrInput).good())
-		chrInput = 'e';
-        std::cin.clear();
-        std::cin.ignore(INT_MAX, '\n');
-
-	return chrInput;
-}
-
-/* ask for the other parameters needed to generate a task */
-void ask_parameters_txt(short & nr_ratios, unsigned int & max_md)
-{
-	CLEAR;
-	short tmp_short;
-	unsigned int tmp_ui;
-
-	output << "\tKBruch " << KBRUCH_VERSION << " " << DATE << endl;
-	output << "\n\n\tEnter number of ratios (between 2 and 5): ";
-
-	/* get the input and check for errors */
-	if (!(std::cin >> tmp_short).good())
-		tmp_short = 2;
-        std::cin.clear();
-        std::cin.ignore(INT_MAX, '\n');
-
-	/* we only allow 2 to 5 ratios per task, we are not able to print more
-	 * on a console in a good form */
-	if (tmp_short < 2)
-		tmp_short = 2;
-	if (tmp_short > 5)
-		tmp_short = 5;
-	nr_ratios = tmp_short; /* save the input */
-
-	output << "\n\n\tEnter the maximum size of the main denominator!";
-	output << "\n\tIt must be at least " << pow(2, nr_ratios) << ": ";
-
-	/* get the input and check for errors */
-	if (!(std::cin >> tmp_ui).good())
-		tmp_ui = 2;
-        std::cin.clear();
-        std::cin.ignore(INT_MAX, '\n');
-
-	/* we only allow 2 to 5 ratios per task, we are not able to print more
-	 * on a console in a good form */
-	if (tmp_ui < pow(2, nr_ratios))
-		tmp_ui = (unsigned int) pow(2, nr_ratios);
-
-	/* we do not allow the maximum denominator to be more then 4 times
-	 * of the required minimum */
-	if (tmp_ui > 4 * pow(2, nr_ratios))
-		tmp_ui = 4 * (unsigned int) pow(2, nr_ratios);
-	max_md = tmp_ui; /* save the input */
-
-	return;
-}
-
-/* generates the next task, asks the user for the result */
-short run_task_txt(short add_sub, short mul_div,
-						short nr_ratios, unsigned int max_md)
-{
-	task klausur;
-	ratio result;
-	int int_num, int_deno;
-	short wrong = FALSE;
-
-	CLEAR;
-	output << "\tKBruch " << KBRUCH_VERSION << " " << DATE << endl;
-	output << "\n\n\tSolve the given task: \n" << endl;
-
-	/* create a new task with the given parameters */
-	klausur.create_task(max_md, nr_ratios, add_sub, mul_div);
-
-	/* we do not want a numerator bigger than 200 for the result */
-	do
-	{
-		klausur.create_task(max_md, nr_ratios, add_sub, mul_div);
-		result = klausur.solve(); /* solve the task */
-if (QABS(result.numerator() > 200))
-	output << "DEBUG MESSAGE zaehler: " << result.numerator() << endl;
-	} while (QABS(result.numerator()) > 200);
-
-	output << qSetW(8) << klausur << endl; /* display the task */
-
-	output << "\n\n\tEnter the numerator: ";
-
-	/* check, if the user entered a valid input */
-	if (!(std::cin >> int_num).good())
-		int_num = 0;
-        std::cin.clear();
-        std::cin.ignore(INT_MAX, '\n');
-
-	output << "\n\tEnter the denominator: ";
-
-	/* check, if the user entered a valid input */
-	if (!(std::cin >> int_deno).good())
-		int_deno = 1;
-        std::cin.clear();
-        std::cin.ignore(INT_MAX, '\n');
-
-	/* if the result is smaller than 0, the minus must be given
-	 * to the numerator */
-	if (int_deno < 0)
-	{
-		int_deno *= -1;
-		int_num *= -1;
-	}
-
-	/* check if the user entered the right result */
-	if (int_num != result.numerator() || int_deno != result.denominator())
-		wrong = TRUE;
-
-	/* we accept the result, if it zero, but we do not accept 0/0 */
-	if (int_num == 0 && result.numerator() == 0 && int_deno != 0)
-		wrong = FALSE;
-
-	/* inform the user, if he entered the right value */
-	if (wrong == TRUE)
-	{
-		CLEAR;
-		output << "\tKBruch " << KBRUCH_VERSION << " " << DATE << endl;
-		output << "\n\n\tYou entered the wrong result for the task:\n" << endl;
-		output << qSetW(8) << klausur << endl; /* display the task */
-		output << "\n\n\tThe correct result is:\n" << endl;
-		output << qSetW(8) << result << endl;
-		output << "\n\n\tpress ENTER to go on: ";
-                std::cin.ignore(INT_MAX, '\n');
-                std::cin.clear();
-		return 0;
-	} else {
-		output << "\n\n\tYes, you entered the correct result." << endl;
-		output << "\tPress ENTER to go on: ";
-                std::cin.ignore(INT_MAX, '\n');
-                std::cin.clear();
-		return 1;
-	}
-}
-
-/* show some more information about the program */
-void show_about_txt()
-{
-	CLEAR;
-	output << "\tKBruch " << KBRUCH_VERSION << " " << DATE << endl;
-	output << "\n\t\tDescription: task generator for calculations with fractions";
-	output << endl << "\n\t\tHomepage: " << HOMEPAGE << endl;
-	output << "\n\t\tMaintainer: " << MAINTAINER << " <" << MAINTAINER_EMAIL;
-	output << ">" << endl << "\n\n\tPress ENTER to go on: ";
-        std::cin.ignore(INT_MAX, '\n');
-        std::cin.clear();
-	return;
-}
-
-/* to show a very simple text interface for console users */
-int gui_konsole(void)
-{
-	task klausur;
-	short add_sub, mul_div;
-	short & nr_ratios = * new short;
-	unsigned int & max_md = * new unsigned int;
-	char chrInput1, chrInput2;
-	unsigned int correct, count;
-
-	/* print the main menu and ask what to do */
-	while ((chrInput1 = show_main_txt()) != 'e')
-	{
-		if (chrInput1 == 'r' || chrInput1 == 'R')
-		{
-			(void) show_about_txt();
-		/* check what kind of operations are wanted for the task */
-		} else {
-			 if (chrInput1 == 'a' || chrInput1 == 'A')
-			{
-				/* only add/sub */
-				add_sub = TRUE;
-				mul_div = FALSE;
-			} else if (chrInput1 == 'm' || chrInput1 == 'M')
-			{
-				/* only mul/div */
-				add_sub = FALSE;
-				mul_div = TRUE;
-			} else {
-				/* add/sub and mul/div */
-				add_sub = TRUE;
-				mul_div = TRUE;
-			}
-			(void) ask_parameters_txt(nr_ratios, max_md);
-
-			correct = 0;
-			count = 0;
-			do
-			{
-				correct += run_task_txt(add_sub, mul_div, nr_ratios, max_md);
-				count++;
-
-				CLEAR;
-				output << "\tKBruch " << KBRUCH_VERSION << " " << DATE << endl;
-				output << "\n\n\ttasks so far: \t" << count << endl;
-				output << "\tcorrect: \t" << correct;
-				output << "  " << int(double(correct) / count * 100) << "%" << endl;
-				output << "\tincorrect: \t" << (count - correct);
-				output << "  " << int(double(count - correct) / count * 100) << "%";
-				output << "\n\n\t\t[n]ext task (default)" << endl;
-				output << "\t\t[c]hange parameters" << endl;
-				output << "\t\t[e]xit program" << endl;
-				output << "\n\tInput: ";
-
-				/* check, if the user entered a valid input */
-				if (!(std::cin >> chrInput2).good())
-					chrInput2 = 'n';
-                                std::cin.clear();
-                                std::cin.ignore(INT_MAX, '\n');
-
-				/* check, if the user wants to change the task parameter */
-				if (chrInput2 == 'c' || chrInput2 == 'C')
-					break;
-
-				/* check, if the user wants to quit the program */
-				if (chrInput2 == 'e' || chrInput2 == 'E')
-					return 0;
-
-				/* user do not want to quit or change parameters, so go on
-				 * with the next task */
-			} while(1);
-		}
-	}
-	return 0;
-}
-
-/* to show a Qt GUI */
-int gui_qt_ini(int argc, char * argv[])
-{
-	/* fill the about data; the common KDE about dialog will show it to the
-	 * user */
-	KAboutData aboutData( "kbruch",
-		description,
-		KBRUCH_VERSION,
-		I18N_NOOP("Automatically generate tasks with fractions"),
-		KAboutData::License_GPL,
-    	"(c) 2002-2004, Sebastian Stein", 0, "http://edu.kde.org/kbruch/",
-		"seb.kde@hpfsc.de");
-	aboutData.addAuthor("Sebastian Stein",0, "seb.kde@hpfsc.de");
- 	KCmdLineArgs::init( argc, argv, &aboutData );
-
-	/* our own options -t and -g are already checked before to prevent
-	 * the generation of a KApplication object */
-	KCmdLineArgs::addCmdLineOptions( options ); // Add our own options.
-
-	KApplication bruch;
-
-	/* !! later we should use a bigger one from the libary -> kid's size */
-	bruch.setFont(KGlobalSettings::generalFont());
-
-	MainQtWidget * bruchApp = new MainQtWidget();
-	bruch.setMainWidget(bruchApp);
-	bruchApp->show();
-
-	return bruch.exec();
-}
+/* these includes are needed for Qt support */
+#include <kapplication.h>
+#include <kcmdlineargs.h>
+#include <kaboutdata.h>
 
 /* the main program */
 int main(int argc, char * argv[])
@@ -307,40 +30,24 @@ int main(int argc, char * argv[])
 	// init random generator
 	srand(time(NULL));
 
-	/* check, if we have some command line parameters */
-	for (int count = 1; count < argc; count++)
-	{
-		/* we react on the -t and -g parameter in this place and all other
-		 * parameters are given to the KDE routines to be checked */
-		if (! strcmp(argv[count], "--text") || !strcmp(argv[count], "-t"))
-		{
-			/* start the kbruch in text mode */
-			return gui_konsole();
-		} else if (! strcmp(argv[count], "-g")
-						|| ! strcmp(argv[count], "--generate"))
-		{
-			task klausur;
-			ratio ergebnis;
-			do
-			{
-				do
-				{
-					klausur.create_task(100, 5, YES, YES);
-					ergebnis = klausur.solve();
-				} while (QABS(ergebnis.numerator()) > 200);
+	/* fill the about data; the common KDE about dialog will show it to the
+	 * user */
+	KAboutData aboutData( "kbruch",
+		description,
+		KBRUCH_VERSION,
+		I18N_NOOP("Learn calculating with fractions."),
+		KAboutData::License_GPL,
+    	"(c) 2002-2004, Sebastian Stein", 0, "http://edu.kde.org/kbruch/",
+		"seb.kde@hpfsc.de");
+	aboutData.addAuthor("Sebastian Stein",0, "seb.kde@hpfsc.de");
+ 	KCmdLineArgs::init( argc, argv, &aboutData );
 
-				output << "Task:" << endl;
-				output << qSetW(8) << klausur << endl;
-				output << "Solution:" << endl;
-				output << qSetW(8) << ergebnis << endl;
-				output << "Press ENTER to go on or stop program ";
-				output << "by pressing Ctrl+C" << endl;
-                                std::cin.get();
-                                std::cin.clear();
-			} while (1);
-		}
-	} /* for (...) -> loop through all cmd parameters */
+	KApplication kbruch;
 
-	/* lets call the Qt GUI; it will handle all cmd options which are left */
-	return gui_qt_ini(argc, argv);
+	MainQtWidget * kbruchApp = new MainQtWidget();
+	kbruch.setMainWidget(kbruchApp);
+	kbruchApp->show();
+
+	/* lets execute the Qt GUI; it will handle all cmd options which are left */
+	return kbruch.exec();
 }
--- src/kbruch.h
+++ src/kbruch.h
@@ -22,80 +22,18 @@
 
 #include <config.h>
 
-#define MAINTAINER "Sebastian Stein"
-#define MAINTAINER_EMAIL "seb.kde@hpfsc.de"
-#define HOMEPAGE "http://edu.kde.org/kbruch/"
-#define DATE "[2004-02-18]"
+/* for the I18N_NOOP macro */
+#include <klocale.h>
 
 #include "task.h"
 #include "mainqtwidget.h"
 
-#include <stdlib.h>
-#include <limits.h>
-
-/* these includes are needed for Qt support */
-#include <kapplication.h>
-#include <kcmdlineargs.h>
-#include <kaboutdata.h>
-#include <kglobalsettings.h>
-
 static const char description[] =
 	I18N_NOOP("KBruch");
-
-
-static KCmdLineOptions options[] =
-{
-	{ "t", 0, 0 },
-	{ "text", I18N_NOOP("Start text mode user interface"), 0 },
-	{ "g", 0, 0 },
-	{ "generate", I18N_NOOP("Generates tasks and solutions without stopping"), 0 },
-	KCmdLineLastOption
-};
-
-/* activate this macro, if you want to build a text gui for MS DOS */
-/*#define MSDOS*/
-
-/* we need this only for MS DOS because of clrscr()*/
-/* under Borland C 5 main menu bar Optionen|Project|Compiler|Definitions
- *	put "MSDOS" into the input box */
-#ifdef MSDOS
-#include <conio.h>
-#define CLEAR clrscr();
-#else
-
-/* and on any other system (Unix) */
-#define CLEAR system("clear");
-#endif /* ifdef MSDOS */
-
-/* for Microsoft Windows 9X/2000/XP/... (WIN32) */
-#ifdef WIN32
-#define CLEAR system("cls");
-#endif
 
 class MainQtWidget;
 class task;
 class ratio;
 class primzahl;
-
-/* ------ function prototypes ------ */
-
-/** show the main menu for a text gui */
-char show_main_txt();
-
-/** ask for the other parameters needed to generate a task */
-void ask_parameters_txt(short & nr_ratios, unsigned int & max_md);
-
-/** generates the next task, asks the user for the result */
-short run_task_txt(short add_sub, short mul_div,
-						short nr_ratios, unsigned int max_md);
-
-/** show some more information about the program */
-void show_about_txt(void);
-
-/** to show a very simple text interface for console users */
-int gui_konsole(void);
-
-/** to show a Qt GUI */
-int gui_qt_ini(int argc, char * argv[]);
 
 #endif /* ifndef BRUCH_H */
